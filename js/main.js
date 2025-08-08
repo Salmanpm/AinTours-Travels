26bcc2f72ef1cd72106d69bb33e3d7870cce1591
Salman <sallusalmanmadoll916@gmail.com>
2025-08-08 23:25:02 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,344 @@
+/*-----------------------------------------------------------------------------------
+
+    Theme Name: Abroad - Immigration and Visa Consulting HTML Template
+    Description: Immigration and Visa Consulting HTML Template
+    Author: Chitrakoot Web
+    Version: 1.0
+
+    /* ----------------------------------
+
+    JS Active Code Index
+            
+        01. Preloader
+        02. Sticky Header
+        03. Scroll To Top
+        04. Parallax
+        05. Wow animation - on scroll
+        06. Resize function
+        07. FullScreenHeight function
+        08. ScreenFixedHeight function
+        09. FullScreenHeight and screenHeight with resize function
+        10. Sliders
+        11. CountUp
+        12. Countdown
+        13. Current Year
+        
+    ---------------------------------- */    
+
+(function($) {
+
+    "use strict";
+
+    var $window = $(window);
+
+        /*------------------------------------
+            01. Preloader
+        --------------------------------------*/
+
+        $('#preloader').fadeOut('normall', function() {
+            $(this).remove();
+        });
+
+        /*------------------------------------
+            02. Sticky Header
+        --------------------------------------*/
+
+        $window.on('scroll', function() {
+            var scroll = $window.scrollTop();
+            var logochange = $(".navbar-brand img");
+            var logodefault = $(".navbar-brand.logodefault img");
+            if (scroll <= 50) {
+                $("header").removeClass("scrollHeader").addClass("fixedHeader");
+                logochange.attr('src', 'img/logos/logo-inner.png');
+                logodefault.attr('src', 'img/logos/logo.png');
+            } 
+            else {
+                $("header").removeClass("fixedHeader").addClass("scrollHeader");
+                logochange.attr('src', 'img/logos/logo.png');
+                logodefault.attr('src', 'img/logos/logo.png');
+            }
+        });
+
+
+        /*------------------------------------
+            03. Scroll To Top
+        --------------------------------------*/
+
+        $window.on('scroll', function() {
+            if ($(this).scrollTop() > 500) {
+                $(".scroll-to-top").fadeIn(400);
+
+            } else {
+                $(".scroll-to-top").fadeOut(400);
+            }
+        });
+
+        $(".scroll-to-top").on('click', function(event) {
+            event.preventDefault();
+            $("html, body").animate({
+                scrollTop: 0
+            }, 600);
+        });
+
+        /*------------------------------------
+            04. Parallax
+        --------------------------------------*/
+
+        // sections background image from data background
+        var pageSection = $(".parallax,.bg-img");
+        pageSection.each(function(indx) {
+
+            if ($(this).attr("data-background")) {
+                $(this).css("background-image", "url(" + $(this).data("background") + ")");
+            }
+        });
+
+        /*------------------------------------
+            05. Wow animation - on scroll
+        --------------------------------------*/
+        
+        var wow = new WOW({
+            boxClass: 'wow', // default
+            animateClass: 'animated', // default
+            offset: 0, // default
+            mobile: false, // default
+            live: true // default
+        })
+        wow.init();
+
+        /*------------------------------------
+            06. Resize function
+        --------------------------------------*/
+
+        $window.resize(function(event) {
+            setTimeout(function() {
+                SetResizeContent();
+            }, 500);
+            event.preventDefault();
+        });
+
+        /*------------------------------------
+            07. FullScreenHeight function
+        --------------------------------------*/
+
+        function fullScreenHeight() {
+            var element = $(".full-screen");
+            var $minheight = $window.height();
+            element.css('min-height', $minheight);
+        }
+
+        /*------------------------------------
+            08. ScreenFixedHeight function
+        --------------------------------------*/
+
+        function ScreenFixedHeight() {
+            var $headerHeight = $("header").height();
+            var element = $(".screen-height");
+            var $screenheight = $window.height() - $headerHeight;
+            element.css('height', $screenheight);
+        }
+
+        /*------------------------------------
+            09. FullScreenHeight and screenHeight with resize function
+        --------------------------------------*/        
+
+        function SetResizeContent() {
+            fullScreenHeight();
+            ScreenFixedHeight();
+        }
+
+        SetResizeContent();
+
+    // === when document ready === //
+    $(document).ready(function(){
+
+        /*------------------------------------
+            10. Sliders
+        --------------------------------------*/
+
+        // countries-carousel
+        $('.service-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            nav: false,
+            dots: true,
+            margin: 30,
+            center: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                }
+            }
+        });
+
+        // countries-carousel2
+        $('.countries-carousel2').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            nav: false,
+            dots: false,
+            margin: 30,
+            center: false,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 2
+                },
+                1200: {
+                    items: 2
+                }
+            }
+        });
+
+        // testimonial-carousel1
+        $('.testimonial-carousel1').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            nav: false,
+            navText: ["<span class='fa-solid fa-arrow-left-long'></span>", "<span class='fa-solid fa-arrow-right-long'></span>"],
+            dots: true,
+            center:false,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                }
+            }
+        });
+
+        // clients03-carousel
+        $('.clients03-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1500,
+            nav: false,
+            dots: false,
+            center:false,
+            margin: 30,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 3
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+            }
+        });
+
+        // Sliderfade
+        $('.slider-fade1').owlCarousel({
+            items: 1,
+            loop:true,
+            dots: true,
+            margin: 0,
+            nav: false,
+            navText: ["<i class='ti-angle-left'></i>", "<i class='ti-angle-right'></i>"],
+            autoplay: true,
+            smartSpeed:1500,
+            mouseDrag:false,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut',
+            responsive: {
+                992: {
+                nav: true,
+                dots: false
+                }
+            }
+            
+        });
+        
+        // Default owlCarousel
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            loop:true,
+            dots: false,
+            margin: 0,
+            autoplay:true,
+            smartSpeed:500
+        });   
+
+        // Slider text animation
+        var owl = $('.slider-fade1');
+        owl.on('changed.owl.carousel', function(event) {
+            var item = event.item.index - 2;     // Position of the current item
+            $('.sm-title').removeClass('animated fadeInUp');
+            $('.title').removeClass('animated fadeInUp');
+            $('p').removeClass('animated fadeInUp');
+            $('a').removeClass('animated fadeInUp');
+            $('.owl-item').not('.cloned').eq(item).find('.sm-title').addClass('animated fadeInUp');
+            $('.owl-item').not('.cloned').eq(item).find('.title').addClass('animated fadeInUp');
+            $('.owl-item').not('.cloned').eq(item).find('p').addClass('animated fadeInUp');
+            $('.owl-item').not('.cloned').eq(item).find('a').addClass('animated fadeInUp');
+        });
+
+        /*------------------------------------
+            11. CountUp
+        --------------------------------------*/
+
+        $('.countup').counterUp({
+            delay: 25,
+            time: 2000
+        });
+
+        /*------------------------------------
+            12. Countdown
+        --------------------------------------*/
+
+        // CountDown for coming soon page
+        $(".countdown").countdown({
+            date: "01 Apr 2026 00:01:00", //set your date and time. EX: 15 May 2025 12:00:00
+            format: "on"
+        });
+
+        /*------------------------------------
+            13. Current Year
+        --------------------------------------*/
+
+        $('.current-year').text(new Date().getFullYear());
+      
+    });
+
+})(jQuery);
